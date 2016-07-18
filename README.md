2f4850411302009cb9ad3110d73b59bc724891e7
Gergo Nagy <gergo.nagy90@yahoo.com>
2016-07-18 22:35:37 +0300

    Swapped out space/tab-sensitive implementation with tolerant regex

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# entourage
-Ansible module for adding environment variables to the .bashrc
+# Automated WP development machine setup
+
+[![Ansible](https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ansible_Logo.png/64px-Ansible_Logo.png)](https://www.ansible.com/)
+
+## Overview
+
+Ansible module for adding, overwriting and/or removing environment variables to/from the .bashrc
+
+## Example
+
+- name: Add environment variable
+  entourage:
+    user: myuser
+    key: MY_VARIABLE
+    value: my_value
+    state: present
+
+- name: Remove environment variable
+  entourage:
+    user: my_user
+    key: MY_VARIABLE
+    value: not_used
+    state: absent
+
+## Technical overview
+
+At first, comparison was strict:
+MY_VAR=myvalue                      -> worked
+MY_VAR = myvalue                    -> didn't work
+
+Now, regex happened, so even:
+         MY_VAR      =    myvalue   -> works
--- entourage.py
+++ entourage.py
@@ -26,22 +26,22 @@ EXAMPLES = '''
 from ansible.module_utils.basic import *
 import re
 
-def entourage_present(data):
-    bashrc_path = "/home/" + data['user'] + "/.bashrc"
+def entourage_present(data, bashrc_path):
+    new_variable_def_regex = "(|[ \\t]+)\\b" + data['key'] + "\\b(|[ \\t]+)=.*"
+    new_variable_regex = "(|[ \\t]+)\\b" + data['key'] + "\\b(|[ \\t]+)=(|[ \\t]+)\\b" + data['value'] + "\\b(|[ \\t]+)"
+    new_variable_def_pattern = re.compile(new_variable_def_regex)
+    new_variable_pattern = re.compile(new_variable_regex)
 
-    # (|[ \t]+)\bMY_VARIABLE\b(|[ \t]+)=(|[ \t]+)\bMY_VALUE\b(|[ \t]+)
-    # change current hardcoded values with regex matching
-    new_variable_definition = data['key'] + "="
-    new_variable = new_variable_definition + data['value'] + "\n"
+    new_variable = data['key'] + "=" + data['value'] + "\n"
 
     present = False
 
     with open(bashrc_path) as bashrc:
         for line in bashrc:
-            if new_variable_definition in line:
+            if new_variable_def_pattern.match(line):
                 # if variable defined
                 present = True
-                if new_variable == line:
+                if new_variable_pattern.match(line):
                     # if variable is the same, do nothing
                     result = {"info": "Variable is in bashrc"}
                     return False, False, result
@@ -59,18 +59,25 @@ def entourage_present(data):
 
 
 
-def entourage_absent(data):
-    bashrc_path = "/home/" + data['user'] + "/.bashrc"
+def entourage_absent(data, bashrc_path):
+    new_variable_def_regex = "(|[ \\t]+)\\b" + data['key'] + "\\b(|[ \\t]+)=.*"
+    new_variable_def_pattern = re.compile(new_variable_def_regex)
 
-    new_variable_definition = data['key'] + "="
+    num = 0
 
     with open(bashrc_path) as bashrc:
         for line in bashrc:
-            if new_variable_definition in line:
-                # if variable defined
+            # if variable defined
+            if new_variable_def_pattern.match(line):
                 replace_in_file(bashrc_path, line, "\n")
-                result = {"info": "Variable removed from bashrc"}
-                return False, True, result
+                num += 1
+
+    if num == 1:
+        result = {"info": "Variable removed from bashrc"}
+        return False, True, result
+    if num > 1:
+        result = {"info": "Variables removed from bashrc", "instances": num}
+        return False, True, result
 
     result = {"info": "Variable was absent in bashrc"}
     return False, False, result
@@ -104,13 +111,16 @@ def main():
     }
 
     module = AnsibleModule(argument_spec=fields)
+
+    bashrc_path = "/home/" + module.params['user'] + "/.bashrc"
+
     is_error, has_changed, result = choice_map.get(
-            module.params['state'])(module.params)
+            module.params['state'])(module.params, bashrc_path)
 
     if not is_error:
         module.exit_json(changed=has_changed, meta=result)
     else:
-        module.fail_json(msg="Error handling environment variable", meta=result)
+        module.fail_json(msg="Error while handling environment variable", meta=result)
 
 
 if __name__ == '__main__':
